b74e93c4d8a841d8a7b3725b9256a2c71c452dfa
cscsilviu89 <80416025+cscsilviu89@users.noreply.github.com>
2022-06-28 17:40:16 +0300

    simple console menu

---
+++ SimpleConsoleMenu/SimpleConsoleMenu/SimpleConsoleMenu/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string raspunsUser;
+
+do 
+{
+    Console.Write("Scrie un cuvant: ");
+    raspunsUser = Console.ReadLine();    
+    if (raspunsUser == "masina")
+    {
+        Console.WriteLine("VRUM VRUM VRUM!!!");
+    }
+    else 
+    {
+        Console.WriteLine($"Ai scris: {raspunsUser}");
+    }
+    
+    
+} while (raspunsUser != "stop");
+
+Console.Clear();
+Console.WriteLine("Ati iesit din program");
+
+
